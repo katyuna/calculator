845a438afe46e8328ac3e7f9876a4040e670bbc2
Ekaterina Zarubina <zarubina.e4@wb.ru>
2024-12-06 20:44:00 +0700

    fix: pom

--- src/test/java/CreateCalculationTest.java
+++ src/test/java/CreateCalculationTest.java
@@ -1,13 +1,14 @@
 import calculator.model.calculation.Calculation;
 import calculator.model.calculation.SpecialTerms;
 import calculator.testdata.TestDataGenerator;
+import io.qameta.allure.Story;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 import utils.PojoJsonParser;
 
 import static io.restassured.RestAssured.given;
 
-public class CreateCalculationTest extends BaseTest{
+public class CreateCalculationTest extends BaseTest {
 
     String endpoint = getEndpoint("/v2/internal/calculation/create");
     TestDataGenerator testDataGenerator = new TestDataGenerator();
@@ -15,30 +16,21 @@ public class CreateCalculationTest extends BaseTest{
 
     @Test
     public void testCreateCalculation() {
-        // Отключаем проверку серта - нужно вынести на уровень выше
-        //RestAssured.config = RestAssured.config()
-        //        .sslConfig(new SSLConfig().relaxedHTTPSValidation());
-
-        System.out.println(endpoint);
-
-        String calculation_id = testDataGenerator.generateCalculationId();
 
         //Генерируем данные для калькуляции
+        String calculation_id = TestDataGenerator.generateCalculationId();
         SpecialTerms specialTerms = new SpecialTerms(1.0, 1.0, 104, 1.0, 1);
         Calculation calculation = new Calculation(calculation_id, "simple", specialTerms);
-        //Преобразуем POJO в JSON
 
-        String calculationJSON = pojoJsonParser.parseToJson(calculation);
-        System.out.println(calculationJSON);
+        //Преобразуем POJO в JSON
+        String calculationJSON = PojoJsonParser.parseToJson(calculation);
+
         //Отправляем запрос
         Response response = given()
                 .header("Content-Type", "application/json")
                 .body(calculationJSON)
                 .when()
                 .post(endpoint);
-        // Получаем тело ответа
-        String responseBody = response.getBody().asString();
-
         // Проверяем статус ответа
         response.then().statusCode(201);
     }
